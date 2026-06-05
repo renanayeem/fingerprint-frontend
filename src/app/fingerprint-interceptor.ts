b240569a5243c2c98687694ef8bb2a21e8845cfc
renanayeem <renanayeem16@gmail.com>
2026-06-05 12:09:59 +0530

    added HTTPonly cookie for fingerprint, SHA-256 Hashing

--- src/app/fingerprint-interceptor.ts
+++ src/app/fingerprint-interceptor.ts
@@ -4,20 +4,16 @@ import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 
 export const fingerprintInterceptor: HttpInterceptorFn = (req, next) => {
-  const fingerprint = window.localStorage.getItem('fingerprint') ?? 'unknown-device';
   const router = inject(Router);
 
   const clonedReq = req.clone({
-    setHeaders: {
-      'X-Client-Fingerprint': fingerprint
-    }
+    withCredentials: true
   });
 
   return next(clonedReq).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
         console.log('401 Unauthorized! Redirecting to login...');
-        localStorage.removeItem('fingerprint');
         router.navigate(['/login']);
       }
       return throwError(() => error);
